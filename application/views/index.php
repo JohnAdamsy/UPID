dad101b5fc5ebd7a36922f6756241ee5032c8518
Mureithi <kelvinmwas@gmail.com>
2014-03-20 12:26:11 +0300

    Google Maps

--- application/views/index.php
+++ application/views/index.php
@@ -1,3 +1,95 @@
+
+<script>
+
+$(document).ready(function() {
+// Note: This example requires that you consent to location sharing when
+// prompted by your browser. If you see a blank space instead of the map, this
+// is probably because you have denied permission for location sharing.
+
+var map;
+
+function detectBrowser() {
+  var useragent = navigator.userAgent;
+  var mapdiv = document.getElementById("map-canvas");
+
+  if (useragent.indexOf('iPhone') != -1 || useragent.indexOf('Android') != -1 ) {
+    mapdiv.style.width = '100%';
+    mapdiv.style.height = '100%';
+  } else {
+    mapdiv.style.width = '600px';
+    mapdiv.style.height = '800px';
+  }
+}
+
+function initialize() {
+  var mapOptions = {
+    zoom: 12
+  };
+  map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+
+  // Try HTML5 geolocation
+  if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = new google.maps.LatLng(position.coords.latitude,
+                                       position.coords.longitude);
+$('.modal').on('shown', function () {
+  google.maps.event.trigger(map, 'resize');
+
+  // also redefine center
+  map.setCenter(pos);
+ 
+});
+     
+
+      map.setCenter(pos);
+      
+      var marker = new google.maps.Marker({
+      position: pos,
+      map: map,
+      title: 'You are here!'
+  });
+    }, function() {
+      handleNoGeolocation(true);
+    });
+    
+    
+  } else {
+    // Browser doesn't support Geolocation
+    handleNoGeolocation(false);
+     var marker = new google.maps.Marker({
+      position: pos,
+      map: map,
+      title: 'Geolocation not supported'
+  });
+  }
+   
+}
+
+function handleNoGeolocation(errorFlag) {
+  if (errorFlag) {
+    var content = 'Error: The Geolocation service failed.';
+  } else {
+    var content = 'Error: Your browser doesn\'t support geolocation.';
+  }
+
+  var options = {
+    map: map,
+    position: new google.maps.LatLng(0,0),
+    content: content
+  };
+
+  var infowindow = new google.maps.InfoWindow(options);
+  map.setCenter(options.position);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+
+
+});
+</script>
+    
 <div class="row">
 	<div class="col-md-8 "><div class="tile ">
 
@@ -39,8 +131,13 @@
 			<h4 class="selected">map</h4>
 			<h4>chart</h4>
 			<h4>list</h4>
+			<!-- Button trigger modal 
+		<button class="btn btn-primary btn-sm" data-toggle="modal" data-target="#myModal">
+ 				 Launch demo modal
+		</button>-->
 		</div>
-		<div id="mapbox">
+		<div id="map-canvas" style="width:100%;height:86%;">
+			
 		</div>
 	</div>
 	</div>
@@ -56,28 +153,23 @@
 	</div>
 </div></div>
 
-<script>
-	var map = L.mapbox.map('mapbox', 'examples.map-vyofok3q').setView([-1.31008, 36.81333], 17);
-
-	L.mapbox.markerLayer({
-		// this feature is in the GeoJSON format: see geojson.org
-		// for the full specification
-		type : 'Feature',
-		geometry : {
-			type : 'Point',
-			// coordinates here are in longitude, latitude order because
-			// x, y is the standard for GeoJSON and many formats
-			coordinates : [36.81333, -1.31008]
-		},
-		properties : {
-			title : 'Strathmore University',
-			description : 'Student Center',
-			// one can customize markers by adding simplestyle properties
-			// http://mapbox.com/developers/simplestyle/
-			'marker-size' : 'small',
-			'marker-color' : '#f0a'
-		}
-	}).addTo(map); 
-</script>
+<!-- Map Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h4 class="modal-title" id="myModalLabel">UPID</h4>
+      </div>
+      <div class="modal-lg" id="map-canvas-lg" style="height:70%;" >
         
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+ 
  
